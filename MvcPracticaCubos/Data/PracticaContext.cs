8c07cfeab8ca0fe04515bec0b7342d457cd97252
amandacrespo <mancreslu@hotmail.com>
2025-02-26 13:09:33 +0100

    Add project files.

---
+++ MvcPracticaCubos/Data/PracticaContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcPracticaCubos.Models;
+
+namespace MvcPracticaCubos.Data
+{
+    public class PracticaContext : DbContext
+    {
+
+        public PracticaContext(DbContextOptions<PracticaContext> options) : base(options)
+        { }
+
+        public DbSet<Cubo> Cubos { get; set; }
+        public DbSet<Compra> Compras { get; set; }
+    }
+}
---
+++ MvcPracticaCubos/Repositories/RepositoryCubos.cs
@@ -0,0 +1,15 @@
+﻿using MvcPracticaCubos.Data;
+
+namespace MvcPracticaCubos.Repositories
+{
+    public class RepositoryCubos
+    {
+        private PracticaContext context;
+
+        public RepositoryCubos(PracticaContext context)
+        {
+            this.context = context;
+        }
+
+    }
+}
